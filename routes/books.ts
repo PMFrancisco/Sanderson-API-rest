dfdabe30cba08c6d24c46bfa3cbb2522e15e5fa1
PMFrancisco <145220390+PMFrancisco@users.noreply.github.com>
2024-09-24 13:44:52 +0000

    feat: added dynamic search with req.query

--- routes/books.ts
+++ routes/books.ts
@@ -3,12 +3,29 @@ import BookModel from "../models/bookModel";
 
 const router = express.Router();
 
-router.get("/", async(req: Request, res: Response) => {
-  const getBooks = await BookModel.find();
-  res.json(getBooks);
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const filter: any = {};
+
+    for (const key in req.query) {
+      if (req.query.hasOwnProperty(key)) {
+        filter[key] = req.query[key];
+      }
+    }
+
+    const getBooks = await BookModel.find(filter);
+    res.status(200).json(getBooks);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 });
 
-router.post("/", async(req: Request, res: Response) => {
+router.get("/:_id", async (req: Request, res: Response) => {
+  const getBook = await BookModel.findById(req.params._id);
+  res.json(getBook);
+});
+
+router.post("/", async (req: Request, res: Response) => {
   try {
     const book = new BookModel({
       title: req.body.title,
@@ -23,8 +40,8 @@ router.post("/", async(req: Request, res: Response) => {
     const savedBook = await book.save();
     res.status(201).json(savedBook);
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(error);
   }
-})
+});
 
 export default router;
